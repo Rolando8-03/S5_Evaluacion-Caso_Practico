83902d0bb3777e2285ef509a75bd487c07406ff3
Rolando8 <remena@uamv.edu.ni>
2026-09-17 12:04:21 -0600

    chore: configura módulos de la aplicación

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,9 +2,9 @@ module ni.edu.uam.s5_evaluacioncaso_practico {
     requires javafx.controls;
     requires javafx.fxml;
 
+    opens ni.edu.uam.s5_evaluacioncaso_practico.controller to javafx.fxml;
 
-    opens ni.edu.uam.s5_evaluacioncaso_practico to javafx.fxml;
-    exports ni.edu.uam.s5_evaluacioncaso_practico;
     exports ni.edu.uam.s5_evaluacioncaso_practico.app;
-    opens ni.edu.uam.s5_evaluacioncaso_practico.app to javafx.fxml;
+    exports ni.edu.uam.s5_evaluacioncaso_practico.model;
+    exports ni.edu.uam.s5_evaluacioncaso_practico.model.enums;
 }
